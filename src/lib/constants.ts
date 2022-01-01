74371863e8deaea6a3c21fd21d5bf869292092fe
feefs <feefs@berkeley.edu>
2022-01-01 15:48:38 -0800

    move locking regex

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,6 @@ import { join } from 'path'
 export const ROOT_DIR = join(__dirname, '..', '..')
 
 export const BMAIL_DOMAIN = 'berkeley.edu'
-export const COURSE_NUMBER_REGEX = /\d+/
 
 export const BERKELEY_BLUE = '#003262'
 export const CALIFORNIA_GOLD = '#FDB515'
