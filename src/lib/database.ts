efeffb9b6514774437a027fcdbf4669b76cf8f6d
feefs <feefs@berkeley.edu>
2022-01-07 02:59:29 -0800

    add backups

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,6 +1,7 @@
 import { container } from '@sapphire/framework'
 import BetterSqlite3 from 'better-sqlite3'
 import { blue, green, red, yellow } from 'colorette'
+import * as cron from 'node-cron'
 
 import fs from 'fs'
 import { join } from 'path'
@@ -10,9 +11,11 @@ export let db: BetterSqlite3.Database
 
 const DB_DIR = join(ROOT_DIR, 'db')
 const DB_FILE = join(DB_DIR, 'data.db')
+const DB_BACKUPS_DIR = join(DB_DIR, 'backups')
 
 export function initializeBetterSqlite3() {
 	if (!fs.existsSync(DB_DIR)) fs.mkdirSync(DB_DIR)
+	if (!fs.existsSync(DB_BACKUPS_DIR)) fs.mkdirSync(DB_BACKUPS_DIR)
 	try {
 		db = BetterSqlite3(DB_FILE, {
 			verbose: (message) => container.logger.info(`[${blue('SQLite3')}] - ${yellow(message)}`)
@@ -27,8 +30,16 @@ export function initializeBetterSqlite3() {
 
 function initializeDatabase() {
 	initializeTables()
+	scheduleBackups()
 }
 
 function initializeTables() {
 	db.prepare('CREATE TABLE IF NOT EXISTS verification_hashes (hash TEXT, timestamp INTEGER)').run()
+}
+
+function scheduleBackups() {
+	cron.schedule('0 0 1 * *', async () => {
+		await db.backup(join(DB_BACKUPS_DIR, `backup-${Date.now()}.db`))
+		container.logger.info(green('SQLite3 database backed up!'))
+	})
 }
